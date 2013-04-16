5aa417e9255cf2095e1d716dc785ef05be97c5c3
Thomas Schubert <funthomas424242@googlemail.com>
2013-04-16 21:39:28 +0200

    testdriver implemented

--- Plugins/openjobs/openjobs.example/storytestdrivers/de/openjobs/entities/StoryBewerbenTestDriver.java
+++ Plugins/openjobs/openjobs.example/storytestdrivers/de/openjobs/entities/StoryBewerbenTestDriver.java
@@ -8,7 +8,7 @@ public class StoryBewerbenTestDriver {
 	public StoryBewerbenTestDriver() {
 		super();
 	}
-	
+
 	@TextSyntax("Der Arbeitgeber erstellt ein Stellenangebot.")
 	public Stellenangebot createStellenangebot() {
 		final Arbeitgeber arbeitgeber = new Arbeitgeber();
@@ -16,17 +16,42 @@ public class StoryBewerbenTestDriver {
 	}
 
 	@TextSyntax("Das Ergebnis ist ein Artefakt.")
-	public void assertNotNull(Object ergebnis){
+	public void assertNotNull(Object ergebnis) {
 		Assert.assertNotNull(ergebnis);
 	}
-	// Die Anzahl der Stellen wird auf 1 begrenzt.
-	// Das Stellenangebot kann auf 1 Stelle(n) vermittelt werden.
-	// Der Arbeitssuchende erstellt ein Stellengesuch.
-	// Das Ergebnis ist ein Artefakt.
-	// Der Arbeitssuchende erstellt eine Bewerbung zu einem Stellenangebot.
-	// Das Ergebnis ist ein Artefakt.
-	// Der Arbeitgeber stellt stellt den Arbeitssuchenden der Bewerbung zum
-	// Stellenangebot ein.
-	// Als Ergebnis wird kein Fehler erwartet.
-	// Der Arbeitssuchende erstellt eine Bewerbung zu einem Stellenangebot.
+
+
+	@TextSyntax("Die Anzahl der Stellen wird auf #1 begrenzt.")
+	public void maxStellen(Integer anzahl) {
+		Assert.assertTrue(false);
+	}
+
+	@TextSyntax("Das Stellenangebot kann auf #1 Stelle(n) vermittelt werden.")
+	public void vermittelbarAufStellen(Integer anzahl) {
+		Assert.assertTrue(false);
+	}
+
+	@TextSyntax("Der Arbeitssuchende erstellt ein Stellengesuch.")
+	public Stellengesuch createStellengesuch() {
+		return new Stellengesuch();
+	}
+
+	@TextSyntax("Der Arbeitssuchende erstellt eine Bewerbung zu einem Stellenangebot.")
+	public Bewerbung createBewerbung(Stellenangebot stellenangebot) {
+		return new Bewerbung();
+	}
+
+	@TextSyntax("Der Arbeitgeber stellt stellt den Arbeitssuchenden der Bewerbung zum Stellenangebot ein.")
+	public void einstellenArbeitssuchenden(Bewerbung bewerbung) {
+		Assert.assertTrue(false);
+	}
+
+
+	@TextSyntax("Als Ergebnis wird kein Fehler erwartet.")
+	public void noException(Object ergebnis) {
+		Assert.assertTrue(false);
+	}
+
+	
+
 }
